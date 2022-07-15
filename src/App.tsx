74adcf4406ca06ddc0d6f6db158bcf06234e3886
cduyminh <cduyminh@gmail.com>
2022-07-15 16:45:06 +0700

    initialize project change

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Game } from "./pages/Game";
+import { Home } from "./pages/Home";
+import { Info } from "./pages/Info";
+import { Report } from "./pages/Report";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home></Home>}></Route>
+				<Route path="info" element={<Info></Info>}></Route>
+				<Route path="game" element={<Game></Game>}></Route>
+				<Route path="report" element={<Report></Report>}></Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
